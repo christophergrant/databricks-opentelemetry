63924cf29f31961c504d63e36392626d26f0a13a
Christopher Grant <christopher.grant@protonmail.com>
2024-03-29 16:28:49 -0700

    Update build.sh

--- databricks-otelcol/build.sh
+++ databricks-otelcol/build.sh
@@ -1,2 +1,2 @@
-env GOOS=linux GOARCH=amd64 $GOPATH/ocb --config builder-config-amd64.yaml
-env GOOS=linux GOARCH=arm64 $GOPATH/ocb --config builder-config-arm64.yaml
+env GOOS=linux GOARCH=amd64 builder --config builder-config-amd64.yaml
+env GOOS=linux GOARCH=arm64 builder --config builder-config-arm64.yaml
